0f0fec0302fc5d9dd781eda6b3d8cba66549cd7e
DONGYUN SHIN <windsdy1@gmail.com>
2023-03-05 01:25:03 +0900

    fix: comment, pin 관계설정 / user 제외

--- db/models/pins.js
+++ db/models/pins.js
@@ -13,18 +13,17 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    // static associate(models) {
-    //   this.belongsTo(models.Users, {
-    //     targetKey: 'userId',
-    //     foreignKey: 'UserId',
-    //     onDelete: 'CASCADE',
-    //   });
-    //   this.belongsTo(models.Posts, {
-    //     targetKey: 'postId',
-    //     foreignKey: 'PostId',
-    //     onDelete: 'CASCADE',
-    //   });
-    // }
+    static associate(models) {
+      this.hasMany(models.Comments, {
+        sourceKey: 'postId',
+        foreignKey: 'postId',
+      });
+      //   this.belongsTo(models.Users, {
+      //     targetKey: 'userId',
+      //     foreignKey: 'UserId',
+      //     onDelete: 'CASCADE',
+      //   });
+    }
   }
 
   Pins.init(
