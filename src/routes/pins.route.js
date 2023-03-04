f731a9bda1aecd66a1a3a087d7ed04877e51a9fc
minenam <nhm6947@gmail.com>
2023-03-04 17:25:44 +0900

    fix: 이미지 생성 API - URI 정정

--- src/routes/pins.route.js
+++ src/routes/pins.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const uploadMiddleware = require('../middlewares/upload.middleware');
 const PinController = require('../controllers/pin.controller');
 const pinController = new PinController();
 
-router.get('/', pinController.getAllPins);
-router.get('/:pinId', pinController.getPin);
-router.post('/', pinController.addPin);
+router.get('/pins', pinController.getAllPins);
+router.get('/pin/:pinId', pinController.getPin);
+router.post('/pin', uploadMiddleware, pinController.addPin);
 // router.put('/:pinId', pinController.updatePin);
 // router.delete('/:pinId', pinController.deletePin);
 
