c7f7e8705c87aa41bb84cfacfb456f053a93e103
minenam <nhm6947@gmail.com>
2023-03-04 17:26:05 +0900

    feat: 회원 임시 생성 API 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { Users } = require("../../models")
+const { Users } = require('../../models');
 
-const pinsRouter = require("./pins.route");
-const commentRouter = require("./comments.route");
+const pinsRouter = require('./pins.route');
+const commentRouter = require('./comments.route');
 
-router.use('/pin', [pinsRouter]);
-router.use('/pins', [pinsRouter]);
+router.use('/', [pinsRouter]);
 router.use('/pin/:pinId/comment', [commentRouter]);
 
 router.get('/', (_req, res) => {
   res.send('정상적으로 요청되었습니다.');
 });
 
-router.post('/', async (req,res)=> {
-  const Id = await Users.create()
+router.post('/', async (req, res) => {
+  const Id = await Users.create();
   return Id;
-})
+});
 
 module.exports = router;
