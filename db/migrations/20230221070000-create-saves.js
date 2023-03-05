0206c9610a88346f1a265552fc6f0a1b68701c7c
DONGYUN SHIN <windsdy1@gmail.com>
2023-03-05 20:45:10 +0900

    feat: pin저장을 위한 Saves 모델만들기,

---
+++ db/migrations/20230221070000-create-saves.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  /**
+   * @param {import("sequelize").QueryInterface} queryInterface - Sequelize Query Interface
+   * @param {import("sequelize")} Sequelize - Sequelize
+   * **/
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Saves', {
+      saveId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      pinId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+  /**
+   * @param {import("sequelize").QueryInterface} queryInterface - Sequelize Query Interface
+   * @param {import("sequelize")} Sequelize - Sequelize
+   * **/
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Saves');
+  },
+};
---
+++ db/models/saves.js
@@ -0,0 +1,65 @@
+'use strict';
+const { Model } = require('sequelize');
+
+/**
+ * @param {import("sequelize").Sequelize} sequelize - Sequelize
+ * @param {import("sequelize").DataTypes} DataTypes - Sequelize Column DataTypes
+ * @return {Model} - Sequelize Model
+ * **/
+module.exports = (sequelize, DataTypes) => {
+  class Saves extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.Users, {
+        targetKey: 'userId',
+        foreignKey: 'userId',
+        onDelete: 'CASCADE',
+      });
+      this.belongsTo(models.Pins, {
+        targetKey: 'pinId',
+        foreignKey: 'pinId',
+        onDelete: 'CASCADE',
+      });
+    }
+  }
+
+  Saves.init(
+    {
+      saveId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      userId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'userId',
+        },
+        onDelete: 'cascade',
+      },
+      pinId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Pins',
+          key: 'pinId',
+        },
+        onDelete: 'cascade',
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Saves',
+      timestamps: false ,
+    }
+  );
+  
+  return Saves;
+};
