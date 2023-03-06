8abc1427755f6642b37967c7e3a3343bb3942a1f
kwakseungmin23 <lookingforfandl@gmail.com>
2023-03-06 12:41:53 +0900

    document: LAP documents deleted for simplifying loginAPIs

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,15 +1,8 @@
 const { express } = require("express")
-const UserController = require("../controllers/user.controller")
 
 const router = express.Router();
-const userController = new UserController();
 
-router.use(session({
-  secret: 'mySecretKey', // set a secret key to sign the session ID cookie
-  resave: false, // don't save session if unmodified
-  saveUninitialized: false // don't create session until something is stored
-}));
+router.post('/login',)
 
-router.post('/login', userController.logIn)
-router.post('/signin', userController.signIn)
-router.post('/logout', userController.logOut)
+router.post('/signin', )
+router.post('/logout',)
