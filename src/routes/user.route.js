dcf85ba1cc43f2ef4e480e7d971e6433c54a5bed
kwakseungmin23 <lookingforfandl@gmail.com>
2023-03-06 16:25:44 +0900

    Style: style fixed

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,52 +1,51 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { Users } = require("../../db/models")
-const bcrypt = require("bcrypt")
-const { Op } = require("sequelize")
-const Joi = require('joi')
+const { Users } = require('../../db/models');
+const bcrypt = require('bcrypt');
+const { Op } = require('sequelize');
+const Joi = require('joi');
 const userSchema = Joi.object({
-      email: Joi.string()
-        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-       password: Joi.string()
-        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-      nickname: Joi.string()
-        .alphanum()
-        .min(3)
-        .max(30)
-        .required(),
-    })
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ['com', 'net'] },
+  }),
+  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+  nickname: Joi.string().alphanum().min(3).max(30).required(),
+});
 
 router.post('/signup', async (req, res) => {
   try {
     const { email, password, nickname } = req.body;
-    await userSchema.validateAsync(req.body) // => validation 'user' with using Joi validator.
-    const hash = await bcrypt.hash(password, 10) // => using hash to make password safer.
+    await userSchema.validateAsync(req.body); // => validation 'user' with using Joi validator.
+    const hash = await bcrypt.hash(password, 10); // => using hash to make password safer.
     const user = await Users.create({ email, password: hash, nickname });
-    return res.status(200).json([{ email: user.email }, { nickname: user.nickname}])
+    return res
+      .status(200)
+      .json([{ email: user.email }, { nickname: user.nickname }]);
   } catch (err) {
-    console.log(err)
+    console.log(err);
     return res.status(400).json({ err: err.message });
   }
-})
+});
 
 router.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await Users.findOne({ where: { email } })
+    const user = await Users.findOne({ where: { email } });
     if (user) {
       const validPassword = await bcrypt.compare(password, user.password);
       if (validPassword) {
-        return res.status(200).json({msg: "login successful"})
+        return res.status(200).json({ msg: 'login successful' });
       } else {
-        return res.status(400).json({msg:"wrong password."})
+        return res.status(400).json({ msg: 'wrong password.' });
       }
     } else {
-       return res.status(400).json({msg:"User not found."})
+      return res.status(400).json({ msg: 'User not found.' });
     }
   } catch (err) {
-    console.log(err)
-    return res.status(400).json({err: err.message})
+    console.log(err);
+    return res.status(400).json({ err: err.message });
   }
-})
+});
 
 module.exports = router;
