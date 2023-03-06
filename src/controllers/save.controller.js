6c2e908f52d2f0e2edc45a7d3ca37b07944e41f7
DONGYUN SHIN <windsdy1@gmail.com>
2023-03-06 14:23:09 +0900

    feat: saveController

--- src/controllers/save.controller.js
+++ src/controllers/save.controller.js
@@ -5,6 +5,9 @@ class SaveController {
   constructor() {
     this.saveService = new SaveService();
   }
+  saveUser = async (req, res, next) => {
+    const userId = Math.floor(Math.random() * 11);
+  }
 }
 
 module.exports = SaveController;
