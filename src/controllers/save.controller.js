079630b5761412408bdf46147084f1754ea5bbd9
DONGYUN SHIN <windsdy1@gmail.com>
2023-03-06 14:51:38 +0900

    feat: saveController

--- src/controllers/save.controller.js
+++ src/controllers/save.controller.js
@@ -6,8 +6,17 @@ class SaveController {
     this.saveService = new SaveService();
   }
   saveUser = async (req, res, next) => {
-    const userId = Math.floor(Math.random() * 11);
-  }
+    logger.info(`SaveController.saveUser Request`);
+    try {
+      const userId = Math.floor(Math.random() * 11);
+      const { pinId } = req.params;
+
+      const save = await this.saveService.saveUser({ userId, pinId });
+      return res.status(201).json(save);
+    } catch (error) {
+      next(error);
+    }
+  };
 }
 
 module.exports = SaveController;
