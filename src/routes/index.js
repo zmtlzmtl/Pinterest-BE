8e72fc255bb4a5d24af98aa1d967b0ac5a923113
kwakseungmin23 <lookingforfandl@gmail.com>
2023-03-06 15:12:33 +0900

    Feat: signIn API, logIn API authentication added

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,13 @@ const { Users } = require('../../db/models');
 
 const pinsRouter = require('./pins.route');
 const commentRouter = require('./comments.route');
+const userRouter = require('./user.route')
 
 router.use('/', [pinsRouter]);
 
 router.use('/pin/:pinId/comment', [commentRouter]);
+
+router.use('/', [userRouter])
 
 router.get('/', (_req, res) => {
   res.send('정상적으로 요청되었습니다.');
