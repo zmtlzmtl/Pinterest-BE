9dcafcb6a707f2e15271352b1491aca7bebe8f96
DONGYUN SHIN <windsdy1@gmail.com>
2023-03-06 15:37:23 +0900

    fix: save 경로 변경

--- src/routes/save.route.js
+++ src/routes/save.route.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const SaveController = require('../controllers/save.controller');
 const saveController = new SaveController();
 
-router.put('/follow/:userId', saveController.savePin);
+router.put('/pins/:pinId', saveController.savePin);
 
 module.exports = router;
