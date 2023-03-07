0d2c2c41db65d24cf4a3d12d6f5604fb49724e05
DONGYUN SHIN <windsdy1@gmail.com>
2023-03-07 17:56:48 +0900

    feat: comment 에서 joi를 이용한 유효성 검사

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -3,9 +3,15 @@ const router = express.Router();
 
 const CommentController = require('../controllers/comment.controller');
 const commentController = new CommentController();
+const commentValidation = require('../../validations/commentValidation');
 
 router.get('/:pinId/comments', commentController.getAllComment);
-router.post('/:pinId/comments', commentController.addComment);
+router.post(
+  '/:pinId/comments',
+  commentValidation.paramCheck,
+  commentValidation.commentCheck,
+  commentController.addComment
+);
 router.delete('/:pinId/comments/:commentId', commentController.deleteComment);
 
 module.exports = router;
