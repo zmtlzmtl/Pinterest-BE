7249f6654f2dba3ec8ca21346ba3815582522be5
minenam <nhm6947@gmail.com>
2023-03-09 10:53:13 +0900

    feat: add LoginMiddleware Pin Create, Delete API

--- src/routes/pin.route.js
+++ src/routes/pin.route.js
@@ -5,16 +5,23 @@ const uploadMiddleware = require('../middlewares/upload.middleware');
 const PinController = require('../controllers/pin.controller');
 const pinController = new PinController();
 const pinValidation = require('../validations/pinValidation');
+const loginMiddleware = require('../middlewares/login.middleware');
 
 router.get('/pins', pinController.getAllPins);
 router.get('/pins/:pinId', pinValidation.paramCheck, pinController.getPin);
 router.post(
   '/pins',
+  loginMiddleware,
   uploadMiddleware,
   pinValidation.pinCheck,
   pinController.addPin
 );
 // router.put('/:pinId', pinValidation.pinCheck, pinController.updatePin);
-// router.delete('/:pinId', pinValidation.pinCheck, pinController.deletePin);
+router.delete(
+  '/:pinId',
+  loginMiddleware,
+  pinValidation.pinCheck,
+  pinController.deletePin
+);
 
 module.exports = router;
