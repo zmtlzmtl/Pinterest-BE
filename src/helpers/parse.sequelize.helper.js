f1e984926a7657ed5bc8dad042503cb40fd73fbf
minenam <nhm6947@gmail.com>
2023-03-09 15:39:08 +0900

    feat: add parseSequelize Flat Object helper

---
+++ src/helpers/parse.sequelize.helper.js
@@ -0,0 +1,13 @@
+module.exports = (model) => {
+  let obj = {};
+
+  for (const [k, v] of Object.entries(model)) {
+    if (k.split('.').length > 1) {
+      const key = k.split('.')[1];
+      obj[key] = v;
+    } else {
+      obj[k] = v;
+    }
+  }
+  return obj;
+};
